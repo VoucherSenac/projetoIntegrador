0e5c7e3d5fcf613df1542dcc7120f5f03654263a
VoucherSenac <senacvoucher@gmail.com>
2025-06-24 19:23:01 -0400

    Add files via upload

---
+++ storage/framework/views/0ca324e79f2546e19c875b47a2d0f0c1.php
@@ -0,0 +1,85 @@
+<?php if (isset($component)) { $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54 = $attributes; } ?>
+<?php $component = App\View\Components\AppLayout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('app-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\App\View\Components\AppLayout::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+     <?php $__env->slot('header', null, []); ?> 
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <?php echo e(__('Lista de Usuários')); ?>
+
+        </h2>
+     <?php $__env->endSlot(); ?>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <p class="mb-4">Olá <strong><?php echo e(Auth::user()->name); ?></strong></p>
+                    <?php if(auth()->user()->level === 'admin'): ?>
+                        <p>Seu nível de acesso atual é <?php echo e(Auth::user()->level); ?></p>
+                    <?php endif; ?>
+                </div>
+
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <div class="p-3 bg-gray-100 rounded-lg mb-4">
+                        <?php echo e($users->links()); ?>
+
+                    </div>
+
+                    <table class="table-auto w-full">
+                        <thead class="text-left bg-gray-100">
+                            <tr>
+                                <th >Nivel</th>
+                                <th class="p-4">Nome</th>
+                                <th>E-mail</th>
+                                <th>Data de cadastro</th>
+
+                                <?php if(auth()->user()->level === 'admin'): ?>
+                                    <th>Ações</th>
+                                <?php endif; ?>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $user): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                <tr class="hover:bg-gray-100">
+                                    <td class="text-center">
+                                        <?php if($user->level === 'admin'): ?>
+                                            <i class="fa-solid fa-user-tie"></i>
+                                        <?php endif; ?>
+                                    </td>
+                                    <td class="p-2"><?php echo e($user->name); ?></td>
+                                    <td><?php echo e($user->email); ?></td>
+                                    <td><?php echo e($user-> created_at); ?></td>
+
+                                    <?php if(auth()->user()->level === 'admin'): ?>
+                                        <td>
+                                            <a href="<?php echo e(route('user.edit', $user->id)); ?>">Editar</a>
+                                        </td>
+                                    <?php endif; ?>
+                                </tr>
+
+                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
+<?php $attributes = $__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
+<?php unset($__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
+<?php $component = $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
+<?php unset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
+<?php endif; ?>
+<?php /**PATH C:\Laravel\voucher10\resources\views/users/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/7734e9252286ad122774fb2629604ceb.php
@@ -0,0 +1,56 @@
+<?php if (isset($component)) { $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54 = $attributes; } ?>
+<?php $component = App\View\Components\AppLayout::resolve([] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('app-layout'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\App\View\Components\AppLayout::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+     <?php $__env->slot('header', null, []); ?> 
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <?php echo e(__('Edição de Usuário')); ?>
+
+        </h2>
+     <?php $__env->endSlot(); ?>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <p class="mb-4">Editando nivel de acesso do usuário <strong><?php echo e($user->name); ?></strong></p>
+                    <p>Nível de acesso atual: <strong><?php echo e($user->level); ?></strong></p>
+                </div>
+
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form action="<?php echo e(route('user.update', $user->id)); ?>" method="post">
+                        <?php echo csrf_field(); ?>
+                        <?php echo method_field('PUT'); ?>
+
+                        <label for="level">Selecione o nível</label>
+                        <select name="level" class="py-1 px-8 rounded">
+                            <option value="" selected disabled>Selecione uma opção</option>
+                            <option value="user">Usuário Comum</option>
+                            <option value="admin">Administrador</option>
+                        </select>
+                        <button type="submit" class="text-black rounded py-2 px-4">
+                            <i class="fa-solid fa-floppy-disk mr-2" ></i> Salvar alterações
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
+<?php $attributes = $__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
+<?php unset($__attributesOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54)): ?>
+<?php $component = $__componentOriginal9ac128a9029c0e4701924bd2d73d7f54; ?>
+<?php unset($__componentOriginal9ac128a9029c0e4701924bd2d73d7f54); ?>
+<?php endif; ?>
+<?php /**PATH C:\Laravel\voucher10\resources\views/users/edit.blade.php ENDPATH**/ ?>
